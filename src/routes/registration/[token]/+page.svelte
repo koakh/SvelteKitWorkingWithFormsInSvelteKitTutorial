71a160dfc3e4d99cf9bf080e4435e77e2b4c16f5
Mário Monteiro <marioammonteiro@gmail.com>
2023-01-25 21:17:09 +0000

    before start implement modal

--- src/routes/registration/[token]/+page.svelte
+++ src/routes/registration/[token]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import RegistrationForm from '$lib/components/RegistrationForm.svelte';
-  import type { ActionData, PageData } from './$types'
+	import type { ActionData, PageData } from './$types';
 	import { ValidateTokenStatus } from './types/enums';
 
-  export let data: PageData
-  export let form: ActionData
+	export let data: PageData;
+	export let form: ActionData;
 
-  $: console.log(form);
+	$: console.log(form);
 </script>
 
 {#if data.status == ValidateTokenStatus.INVALID}
@@ -14,12 +14,12 @@
 {:else if data.status == ValidateTokenStatus.EXPIRED}
 	<h1>EXPIRED TOKEN</h1>
 {:else}
-  <!-- required to pass form data to component -->
-	<RegistrationForm form={form}/>
+	<!-- required to pass form data to component -->
+	<RegistrationForm {form} />
 {/if}
 
 {#if data?.countries}
-  {#each data.countries as item}
-    <p>{item.name}</p>
-  {/each}
+	{#each data.countries as item}
+		<p>{item.name}</p>
+	{/each}
 {/if}
